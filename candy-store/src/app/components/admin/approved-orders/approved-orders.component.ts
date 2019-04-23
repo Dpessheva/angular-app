cb0114a1793690631765f0a719b5edda25e2ed85
DPesheva <dpessheva@gmail.com>
2019-04-23 20:33:47 +0300

    fixes

--- candy-store/src/app/components/admin/approved-orders/approved-orders.component.ts
+++ candy-store/src/app/components/admin/approved-orders/approved-orders.component.ts
@@ -9,48 +9,45 @@ import { OrderModel } from '../../orders/models/OrderModel';
 import { OrdersService } from '../../../core/services/orders/orders.service';
 import { UndoOrdersRequestMade } from '../../../core/store/http/http.actions';
 
-
 @Component({
   selector: 'app-approved-orders',
   templateUrl: './approved-orders.component.html',
   styleUrls: ['./approved-orders.component.scss']
 })
 export class ApprovedOrdersComponent extends BaseComponent implements OnInit {
+  protected pageSize: number = 5
+  protected currentPage: number = 1
+  protected notFoundMessage = 'There are no approved orders at the moment.'
+  protected getTotalSum = getTotalSum
+  protected toLocaleString = toLocaleString
+  protected approvedOrders: OrderModel[]
+  private subscription$: Subscription
 
-// tslint:disable-next-line: no-inferrable-types
-  protected pageSize: number = 5;
-// tslint:disable-next-line: no-inferrable-types
-  protected currentPage: number = 1;
-  protected NotFoundMessage = 'There are no approved orders at the moment';
-  protected getTotalSum = getTotalSum;
-  protected toLocaleString = toLocaleString;
-  protected approvedOrders: OrderModel[];
-  private subscription$: Subscription;
-  
   constructor(
     private store: Store<AppState>,
-    private OrdersService: OrdersService) {
-    super();
+    private ordersService: OrdersService ) {
+    super()
   }
 
   ngOnInit() {
-    this.store.dispatch(new UndoOrdersRequestMade());
-    this.OrdersService.getApprovedOrders();
-    this.subscription$ = this.store.pipe(select(state => {
-      if (state.http.ordersRequestMade) {
-        this.approvedOrders = state.orders.approvedOrders
-        .sort((a: OrderModel, b: OrderModel) => +new Date(b.date) - +new Date(a.date));
-      }
-    })
+    this.store.dispatch(new UndoOrdersRequestMade())
+    this.ordersService.getApprovedOrders()
+    this.subscription$ = this.store
+      .pipe(select(state => state))
+      .subscribe(state => {
+        if (state.http.ordersRequestMade) {
+          this.approvedOrders = state.orders.approvedOrders.sort((a: OrderModel, b: OrderModel) => +new Date(b.date) - +new Date(a.date))
+        }
+      })
 
-    );
-    this.subscriptions.push(this.subscription$);
-  }
-  changePage(page) {
-    this.currentPage = page;
+    this.subscriptions.push(this.subscription$)
   }
 
-  trackByIds(index: number, order: OrderModel): string{
-    return order._id;
+  changePage (page) {
+    this.currentPage = page
+  }
+
+  trackByIds(index: number, order: OrderModel): string {
+    return order._id
   }
 }
