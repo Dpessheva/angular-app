74e459cf43f1e5cd9bf6101215cd788ab36800a5
DPesheva <dpessheva@gmail.com>
2019-04-23 15:52:11 +0300

    update

--- candy-store/src/app/components/shop/shop.component.spec.ts
+++ candy-store/src/app/components/shop/shop.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { StoreComponent } from './store.component';
+import { StoreComponent } from './shop.component';
 
 describe('StoreComponent', () => {
   let component: StoreComponent;
